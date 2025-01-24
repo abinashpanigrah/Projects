c385f1036174a945df0ac815accac5bba6ea528e
AbinashPanigrahi <121704518+abinashpanigrah@users.noreply.github.com>
2025-01-24 20:10:30 +0530

    Update Employee.java

--- Employee_Hub/src/main/java/com/employee/entities/Employee.java
+++ Employee_Hub/src/main/java/com/employee/entities/Employee.java
@@ -15,7 +15,7 @@ public class Employee
 {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
+        private long id;
 	
 	@Column(name="employee_id", nullable=false, unique=true)
 	private String employeeId;
@@ -49,106 +49,113 @@ public class Employee
 	@DateTimeFormat(pattern = "yyyy-MM-dd")
 	private Date joiningDate;
 
-	public long getId() {
-		return id;
+	public long getId() 
+	{
+	         return id;
 	}
 
-	public void setId(long id) {
+	public void setId(long id) 
+	{
 		this.id = id;
 	}
 
-	public String getEmployeeId() {
+	public String getEmployeeId()
+	{
 		return employeeId;
 	}
 
-	public void setEmployeeId(String employeeId) {
+	public void setEmployeeId(String employeeId) 
+	{
 		this.employeeId = employeeId;
 	}
 
-	public String getFirstName() {
+	public String getFirstName() 
+	{
 		return firstName;
 	}
 
-	public void setFirstName(String firstName) {
+	public void setFirstName(String firstName)
+	{
 		this.firstName = firstName;
 	}
 
-	public String getLastName() {
+	public String getLastName() 
+	{
 		return lastName;
 	}
 
-	public void setLastName(String lastName) {
+	public void setLastName(String lastName)
+	{
 		this.lastName = lastName;
 	}
 
-	public String getGender() {
+	public String getGender()
+	{
 		return gender;
 	}
 
-	public void setGender(String gender) {
+	public void setGender(String gender)
+	{
 		this.gender = gender;
 	}
 
-	public Date getDateOfBirth() {
+	public Date getDateOfBirth()
+	{
 		return dateOfBirth;
 	}
 
-	public void setDateOfBirth(Date dateOfBirth) {
+	public void setDateOfBirth(Date dateOfBirth)
+	{
 		this.dateOfBirth = dateOfBirth;
 	}
 
-	public String getPhoneNumber() {
+	public String getPhoneNumber()
+	{
 		return phoneNumber;
 	}
 
-	public void setPhoneNumber(String phoneNumber) {
+	public void setPhoneNumber(String phoneNumber)
+	{
 		this.phoneNumber = phoneNumber;
 	}
 
-	public String getEmail() {
+	public String getEmail() 
+	{
 		return email;
 	}
 
-	public void setEmail(String email) {
+	public void setEmail(String email)
+	{
 		this.email = email;
 	}
 
-	public String getAddress() {
+	public String getAddress() 
+	{
 		return address;
 	}
 
-	public void setAddress(String address) {
+	public void setAddress(String address) 
+	{
 		this.address = address;
 	}
 
-	public String getJobTitle() {
+	public String getJobTitle() 
+	{
 		return jobTitle;
 	}
 
-	public void setJobTitle(String jobTitle) {
+	public void setJobTitle(String jobTitle) 
+	{
 		this.jobTitle = jobTitle;
 	}
 
-	public Date getJoiningDate() {
+	public Date getJoiningDate() 
+	{
 		return joiningDate;
 	}
 
-	public void setJoiningDate(Date joiningDate) {
+	public void setJoiningDate(Date joiningDate) 
+	{
 		this.joiningDate = joiningDate;
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
 }
